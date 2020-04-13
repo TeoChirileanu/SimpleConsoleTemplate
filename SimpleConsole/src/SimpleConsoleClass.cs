303fbd57781a64fa3632acd1ee615569964d2a6a
Teodor <teodorchirileanu@thecarpathiancoder.dev>
2020-04-13 20:46:37 +0200

    init

---
+++ SimpleConsole/src/SimpleConsoleClass.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SimpleConsoleProject
+{
+    internal static class SimpleConsoleClass
+    {
+        private static void Main() => Console.WriteLine("Hello World!");
+    }
+}
